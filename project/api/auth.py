75dbc25803686991be0406b0c8f58937ec8701fe
Edi Santoso <repopamor@gmail.com>
2018-02-17 12:25:52 +0700

    part3: Auth Routes - Tests Register Route

--- project/api/auth.py
+++ project/api/auth.py
@@ -9,3 +9,51 @@ from project import db, bcrypt
 
 auth_blueprint = Blueprint('auth', __name__)
 
+
+@auth_blueprint.route('/auth/register', methods=['POST'])
+def register_user():
+	# get post data
+	post_data = request.get_json()
+	if not post_data:
+		response_object = {
+			'status': 'error',
+			'message': 'Invalid payload.'
+		}
+		return jsonify(response_object), 400
+	username = post_data.get('username')
+	email = post_data.get('email')
+	password = post_data.get('password')
+	try:
+		# check for existing user
+		user = User.query.filter(
+			or_(User.username == username, User.email == email)).first()
+		if not user:
+			new_user = User(
+				username=username,
+				email=email,
+				password=password
+			)
+			db.session.add(new_user)
+			db.session.commit()
+			# generate auth token
+			auth_token = new_user.encode_auth_token(new_user.id)
+			response_object = {
+				'status': 'success',
+				'message': 'Successfully registered.',
+				'auth_token': auth_token.decode()
+			}
+			return jsonify(response_object), 201
+		else:
+			response_object = {
+				'status': 'error',
+				'message': 'Sorry. That user already exists.'
+			}
+			return jsonify(response_object), 400
+	# handle errors
+	except (exc.IntegrityError, ValueError) as e:
+		db.session.rollback()
+		response_object = {
+			'status': 'error',
+			'message': 'Invalid payload.'
+		}
+		return jsonify(response_object), 400
--- project/tests/test_auth.py
+++ project/tests/test_auth.py
@@ -9,4 +9,74 @@ from project.tests.utils import add_user
 
 
 class TestAuthBlueprint(BaseTestCase):
-	pass
+
+	def test_user_registration(self):
+		with self.client:
+			response = self.client.post(
+				'/auth/register',
+				data=json.dumps(dict(
+					username='testuser',
+					email='user@test.com',
+					password='123456'
+				)),
+				content_type='application/json'
+			)
+			data = json.loads(response.data.decode())
+			self.assertTrue(data['status'] == 'success')
+			self.assertTrue(data['message'] == 'Successfully registered.')
+			self.assertTrue(data['auth_token'])
+			self.assertTrue(response.content_type == 'application/json')
+			self.assertEqual(response.status_code, 201)
+
+	def test_user_registration_duplicate_email(self):
+		add_user('testuser2', 'user@test.com', '123456')
+		with self.client:
+			response = self.client.post(
+				'/auth/register',
+				data=json.dumps(dict(
+					username='testuser',
+					email='user@test.com',
+					password='123456'
+				)),
+				content_type='application/json'
+			)
+			data = json.loads(response.data.decode())
+			self.assertEqual(response.status_code, 400)
+			self.assertIn('Sorry. That user already exists.', data['message'])
+			self.assertIn('error', data['status'])
+
+	def test_user_registration_invalid_json(self):
+		with self.client:
+			response = self.client.post(
+				'/auth/register',
+				data=json.dumps(dict()),
+				content_type='application/json'
+			)
+			data = json.loads(response.data.decode())
+			self.assertEqual(response.status_code, 400)
+			self.assertIn('Invalid payload.', data['message'])
+			self.assertIn('error', data['status'])
+
+	def test_user_registration_invalid_json_keys_no_username(self):
+		with self.client:
+			response = self.client.post(
+				'/auth/register',
+				data=json.dumps(dict(email='user@test.com', password='1234')),
+				content_type='application/json'
+			)
+			data = json.loads(response.data.decode())
+			self.assertEqual(response.status_code, 400)
+			self.assertIn('Invalid payload.', data['message'])
+			self.assertIn('error', data['status'])
+
+	def test_user_registration_invalid_json_keys_no_email(self):
+		with self.client:
+			response = self.client.post(
+				'/auth/register',
+				data=json.dumps(dict(username='testuser', password='1234')),
+				content_type='application/json'
+			)
+			data = json.loads(response.data.decode())
+			self.assertEqual(response.status_code, 400)
+			self.assertIn('Invalid payload.', data['message'])
+			self.assertIn('error', data['status'])
